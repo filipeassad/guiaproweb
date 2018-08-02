dc86999bed29fffd9c66a7c48b2926341fef7f08
Filipe Assad <3647@ucdb.br>
2018-08-02 13:24:05 -0400

    Servidor de notificação iniciado.

---
+++ socket/controllers/NotificacaoController.js
@@ -0,0 +1,4 @@
+'use strict';
+exports.enviar_notificacao = (serverSocket, socketIO, idAtendimento) =>{    
+
+}
---
+++ socket/routes/NotificacaoRoutes.js
@@ -0,0 +1,5 @@
+'use strict';
+module.exports = function(app) {
+    var autenticacao = require('../../configs/autenticacao.js');
+    var permissao = require('../../configs/permissao.js');
+};
