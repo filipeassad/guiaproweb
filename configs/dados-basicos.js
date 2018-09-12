ee95035431c771fd56028e07b8d018712597aefb
Filipe Assad <filipeassad@gmail.com>
2018-09-12 19:05:23 -0400

    Remoção dos construtores.

--- configs/dados-basicos.js
+++ configs/dados-basicos.js
@@ -1,5 +1,4 @@
 const db = require('./dbConfig.js');
-const construtores = require('../api/construtores');
 const variaveisCadastro = require('./variaveiscadastro');
 const Usuario = db.usuario;
 const Perfil = db.perfil;
@@ -60,14 +59,14 @@ function cadastrarSituacoes(){
 }
 
 function cadastrarUsuario(usuarioB){
-    return Usuario.create(new construtores.UsuarioObj(usuarioB)).then(function (usuario) {
-        Endereco.create(new construtores.EnderecoObjByUsuario(usuarioB)).then(function (endereco) {
+    return Usuario.create(new UsuarioObj(usuarioB)).then(function (usuario) {
+        Endereco.create(new EnderecoObjByUsuario(usuarioB)).then(function (endereco) {
             usuarioB.id = usuario.id;
             usuarioB.perfil.endereco.id = endereco.id;
-            Perfil.create(new construtores.PerfilObjByUsuario(usuarioB)).then(function (perfil) {
+            Perfil.create(new PerfilObjByUsuario(usuarioB)).then(function (perfil) {
                 usuarioB.perfil.id = perfil.id;
                 db.sequelize.Promise.map(usuarioB.perfil.permissoes, function (permissao) {
-                    PermissaoPerfil.create(new construtores.PerfilPermissaoObj(usuarioB, permissao));
+                    PermissaoPerfil.create(new PerfilPermissaoObj(usuarioB, permissao));
                 });
             });
         });
@@ -75,13 +74,64 @@ function cadastrarUsuario(usuarioB){
 }
 
 function cadastrarSitiacao(situacaoB){
-    return Situacao.create(new construtores.SituacaoObj(situacaoB));
+    return Situacao.create(new SituacaoObj(situacaoB));
 }
 
 function cadastrarTipoPerfil(tipoperfilB){
-    return TipoPerfil.create(new construtores.TipoPerfilObj(tipoperfilB));
+    return TipoPerfil.create(new TipoPerfilObj(tipoperfilB));
 }
 
 function cadastrarPermissao(permissaoB){
-    return Permissao.create(new construtores.PermissaoObj(permissaoB));
+    return Permissao.create(new PermissaoObj(permissaoB));
+}
+
+function SituacaoObj(situacao){
+    this.descricao = situacao.descricao;
+}
+
+function UsuarioObj(usuario){
+	this.email = usuario.email;
+	this.senha = usuario.senha;
+}
+
+function EnderecoObjByUsuario(usuario){
+	this.cep = usuario.perfil.endereco.cep;
+	this.numero = usuario.perfil.endereco.numero;
+	this.logradouro = usuario.perfil.endereco.logradouro;
+	this.complemento = usuario.perfil.endereco.complemento;
+	this.bairro = usuario.perfil.endereco.bairro;
+	this.cidade = usuario.perfil.endereco.cidade;
+	this.uf = usuario.perfil.endereco.uf;
+	this.pais = usuario.perfil.endereco.pais;
+	this.latitude = usuario.perfil.endereco.latitude;
+	this.longitude = usuario.perfil.endereco.longitude;
+}
+
+function PerfilObjByUsuario(usuario){
+	this.nome = usuario.perfil.nome;
+	this.sobrenome = usuario.perfil.sobrenome;
+	this.datanascimento = usuario.perfil.datanascimento;
+	this.cpf = usuario.perfil.cpf;
+	this.sexo = usuario.perfil.sexo;
+	this.celular = usuario.perfil.celular;
+	this.urlimg = usuario.perfil.urlimg;
+	this.ativo = usuario.perfil.ativo;
+	this.usuarioId = usuario.id;
+	this.tipoperfilId = usuario.perfil.tipoperfilId;
+	this.enderecoId = usuario.perfil.endereco.id;
+}
+
+function PerfilPermissaoObj(usuario, permissao){
+	this.perfilId = usuario.perfil.id;
+	this.permissaoId = permissao.permissaoId;
+}
+
+function TipoPerfilObj(tipoperfil){
+	this.descricao = tipoperfil.descricao;
+	this.sigla = tipoperfil.sigla;
+}
+
+function PermissaoObj(permissao){
+	this.descricao = permissao.descricao;
+	this.sigla = permissao.sigla;
 }
