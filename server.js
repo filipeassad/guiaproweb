cb93c74adf35605106332f5cf90cddec836254d3
Filipe Assad <filipeassad@gmail.com>
2018-10-15 21:48:29 -0400

    Adicionando o Cors

--- server.js
+++ server.js
@@ -7,6 +7,12 @@ var cookieParser = require('cookie-parser');
 app.use(bodyParser.json());
 app.use(cookieParser());
 
+app.use(function(req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header("Access-Control-Allow-Headers", "Origin, X-Requested-With, Content-Type, Accept");
+  next();
+});
+
 var server = app.listen(port, function(){
     var host = server.address().address;
     var port = server.address().port;
