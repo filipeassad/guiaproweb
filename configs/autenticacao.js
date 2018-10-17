8b694425bcd4058270c8512a1ae58f88d5b5fffd
Filipe Assad <filipeassad@gmail.com>
2018-10-16 22:49:28 -0400

    correção

--- configs/autenticacao.js
+++ configs/autenticacao.js
@@ -38,9 +38,6 @@ exports.sucessoLogin = (req, res) => {
 
 exports.validaToken = function (req, res, next){
 
-    console.log('Passou');
-    console.log(req.headers['x-access-token']);
-
     var token = req.headers['x-access-token'];
     
     if(token == null){
