5cfb5c6c230e8fed5bba63652b3e372ffb6b3239
Filipe Assad <filipeassad@gmail.com>
2018-12-13 18:29:31 -0300

    Alteração busca perfil

--- api/controllers/PerfilController.js
+++ api/controllers/PerfilController.js
@@ -131,7 +131,8 @@ exports.obter_perfil_pela_categoria = (req, res) =>{
                     as:'categorias', 
                     where: {categoriaId:categoriaId}, 
                     include:[{model: Categoria, as: 'categoria'}]
-                }             
+				},
+				{where: {ativo: 'true'}}             
             ]                       
         }).then(perfils => {
 		res.send(perfils);
