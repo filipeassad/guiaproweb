698fff5cf2f0be2efd0b677df02f25a350350d50
Filipe Assad <filipeassad@gmail.com>
2018-12-17 20:49:32 -0300

    Console pra achar o erro

--- api/controllers/PermissaoController.js
+++ api/controllers/PermissaoController.js
@@ -32,8 +32,9 @@ exports.deletar_permissao = (req, res) => {
 
 exports.obter_todos_permissaos = (req, res) => {
 	var usuarioId = req.decoded.id;
-
+	console.log(req.decoded.id);
 	Usuario.findById(usuarioId, { include: [{ all: true, nested: true }] }).then(usuario => {
+		console.log(usuario);
 		var cond = montarCondicao(usuario.perfil.permissoes[0].id);
 		Permissao.findAll({
 			include: [{all: true, nested: true}],
