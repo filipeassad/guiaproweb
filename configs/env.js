adeb4dc66340ff6dc35c89b1287c3dc5d041bf7e
Filipe Assad <filipeassad@gmail.com>
2018-12-17 21:45:22 -0300

    apontando para base de produção

--- configs/env.js
+++ configs/env.js
@@ -1,8 +1,23 @@
-const env = {
+
+/*const env = {
     database: 'd77l40qp3mh73s',
     username: 'wuhqxrikmisxmr',
     password: 'b0aaf0aa3198537749f89e64b24179a3155410529b19e0e7becfb277cb416c32',
     host: 'ec2-54-221-210-97.compute-1.amazonaws.com',
+    dialect: 'postgres',
+    pool: {
+        max: 5,
+        min: 0,
+        acquire: 30000,
+        idle: 10000
+    }
+};*/
+
+const env = {
+    database: 'd6p4gpu42gma79',
+    username: 'eamgqroqbkfjwn',
+    password: 'f18f8bbe15a2c1cfeaa08d9f4499dd2d1d646aad1461c387630791c5aacf388c',
+    host: 'ec2-23-21-122-141.compute-1.amazonaws.com',
     dialect: 'postgres',
     pool: {
         max: 5,
