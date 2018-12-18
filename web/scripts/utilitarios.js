91bd47141a7a7cbccf81be17fe4fa50b1c81a785
Filipe Assad <filipeassad@gmail.com>
2018-12-17 21:57:30 -0300

    Zerando o banco de dados

--- web/scripts/utilitarios.js
+++ web/scripts/utilitarios.js
@@ -1,2 +1,3 @@
 //var url_principal = "http://localhost:3000/";
-var url_principal = "https://guiaproweb.herokuapp.com/";
+//var url_principal = "https://guiaproweb.herokuapp.com/";
+var url_principal = "https://guiapro.herokuapp.com/";
