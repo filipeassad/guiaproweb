29ac198aa312840ae49cc203af74d71a4db5ba57
Filipe Assad <filipeassad@gmail.com>
2018-12-17 21:12:32 -0300

    Correção Tipo perfil

--- api/controllers/TipoPerfilController.js
+++ api/controllers/TipoPerfilController.js
@@ -1,5 +1,7 @@
 const db = require('../../configs/dbConfig.js');
 const TipoPerfil = db.tipoperfil;
+const Usuario = db.usuario;
+const Op = db.Sequelize.Op;
  
 exports.cadastrar_tipoperfil = (req, res) => {
 	var tipoperfilB = req.body;
@@ -34,20 +36,44 @@ exports.deletar_tipoperfil = (req, res) => {
 };
 
 exports.obter_todos_tipoperfils = (req, res) => {
-	TipoPerfil.findAll({include: [{all: true, nested: true}]}).then(tipoperfils => {
-	  res.send(tipoperfils);
+	var usuarioId = req.decoded.id;
+	Usuario.findById(usuarioId, { include: [{ all: true, nested: true }] }).then(usuario => {
+		var cond = montarCondicao(usuario.perfil.permissoes[0].id);
+		TipoPerfil.findAll({
+			include: [{all: true, nested: true}],
+			where: cond,
+		}).then(tipoperfils => {
+			res.send(tipoperfils);
+		});
 	});
+	
 };
  
-exports.obter_tipoperfil_por_id = (req, res) => {	
+exports.obter_tipoperfil_por_id = (req, res) => {		
 	TipoPerfil.findById(req.params.tipoperfilId, {include: [{all: true, nested: true}]}).then(tipoperfil => {
 		res.send(tipoperfil);
-	})
+	});
 };
 
 function TipoPerfilObj(tipoperfil){
 	this.descricao = tipoperfil.descricao;
 	this.sigla = tipoperfil.sigla;
+}
+
+function montarCondicao(permissao){
+	condicao = {};
+	if(permissao == 1){
+		condicao.id = { [Op.or]: [1,2] };
+	}else if(permissao == 2){
+		condicao.id = { [Op.or]: [1, 2, 3] }
+	}else if(permissao == 3){
+		condicao.id = { [Op.or]: [1,2] }
+	}else if(permissao == 4){
+		condicao.id = { [Op.or]: [2] }
+	}else if(permissao == 5){
+		condicao.id = { [Op.or]: [1] }
+	}
+	return condicao;
 }
 
 function validaTipoPerfil(tipoperfil){
