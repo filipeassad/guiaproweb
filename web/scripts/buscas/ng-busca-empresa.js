0cf65d08c9563f4497802c8d59859e1b30e85628
Filipe Assad <filipeassad@gmail.com>
2019-01-12 17:31:08 -0300

    Criando tela de edição de empresa

--- web/scripts/buscas/ng-busca-empresa.js
+++ web/scripts/buscas/ng-busca-empresa.js
@@ -48,7 +48,7 @@ app.controller('BuscaEmpresaCtrl', [
     }
 
     $scope.editar = function(empresa){
-        httpService.posthttp(url_principal + "/alterar-empresa", empresa.id);
+        httpService.posthttp(url_principal + "alterar-empresa", empresa.id);
     }
 
     $scope.excluir = function(empresa){
