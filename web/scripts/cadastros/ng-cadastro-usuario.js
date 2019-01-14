fca457d081402de9f429b8dc44a9ca5b20983d10
Filipe Assad <filipeassad@gmail.com>
2019-01-13 22:42:21 -0300

    Criar tela de alteração de usuário.

--- web/scripts/cadastros/ng-cadastro-usuario.js
+++ web/scripts/cadastros/ng-cadastro-usuario.js
@@ -24,6 +24,7 @@ app.controller('CadastroUsuarioCtrl', [
         $scope.usuario = {
             perfil:{
                 tipoperfil: {
+                    id: null,
                     descricao: ""
                 },
                 categorias: [], 
